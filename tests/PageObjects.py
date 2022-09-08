97665b917bc267b8fa62419ec0343cd115824044
marisarze <flypetrov@yandex.ru>
2022-09-08 20:35:45 +0300

    raw commit 2

--- tests/PageObjects.py
+++ tests/PageObjects.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,7 +20,8 @@ class BasePage:
         element.clear()
         element.send_keys(value)
 
-    def element_in_element(self, parent_locator: tuple, child_locator: tuple):
+    def element_in_element(self, parent_locator, child_locator: tuple):
+        #if isinstance(parent_locator, 
         return self.element(parent_locator).find_element(*child_locator)
 
     def element(self, locator: tuple):
@@ -50,6 +51,85 @@ class BasePage:
         except TimeoutException:
             raise AssertionError(f"Didn't wait: {method.__name__} done")
 
+class CatalogPage(BasePage):
+    REFINE_SEARCH_DIV = (By.CSS_SELECTOR, "#content > h3")
+    SORT_DROPDOWN = (By.CSS_SELECTOR, "#input-sort")
+    PRODUCT = (By.CSS_SELECTOR, "#div.product-thumb")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "span.price-new")
+    SHOPPING_CART = (By.CSS_SELECTOR, "#header-cart > div > button")
+    TO_CART_BUTTON = (By.CSS_SELECTOR, "button[aria-label='Add to Cart']")
+
+    def sort_by_price_high_to_low(self):
+        sorting_select = Select(self.element(self.SORT_DROPDOWN))
+        sorting_select.select_by_visible_text('Price (High > Low)')
+    
+    def get_product_price(self, product):
+        return float(self.element_in_element(product, self.PRODUCT_PRICE).text.replace(",", "")[1:])
+
+    def get_total(self):
+        return float(self.element(self.SHOPPING_CART).text.split("$")[1].replace(",", ""))
+
+    def add_to_cart(self, product):
+        to_cart_button = self.element_in_element(product, self.TO_CART_BUTTON)
+        self.click(to_cart_button)
+
+
+class LoginPage(BasePage):
+    NEW_CUSTOMER_HEAD = (By.CSS_SELECTOR, "#content > div > div:nth-child(1) > div > h2")
+    RETURNING_CUSTOMER_HEAD = (By.CSS_SELECTOR, "#content > div > div:nth-child(2) > div > h2")
+    EMAIL_INPUT = (By.CSS_SELECTOR, '#input-email')
+    PASSWORD_INPUT = (By.CSS_SELECTOR, '#input-password')
+    LOGIN_CONFIRM = (By.CSS_SELECTOR, "input['value'='Login']")
+
+    def login(self, email, password):
+        email_input = self.element(self.EMAIL_INPUT)
+        self._input(email_input, email)
+        password_input = self.element(self.PASSWORD_INPUT)
+        self._input(password_input, password)
+        self.click(self.element(self.LOGIN_CONFIRM))
+
+
+class AfterLoginPage(BasePage):
+    pass
+
+class RegisterPage(BasePage):
+    PAGE_NAME = (By.CSS_SELECTOR, "#content > h1")
+    FIRST_NAME_INPUT = (By.CSS_SELECTOR, '#input-firstname')
+    LAST_NAME_INPUT = (By.CSS_SELECTOR, '#input-lastname')
+    EMAIL_INPUT = (By.CSS_SELECTOR, '#input-email')
+    PHONE_INPUT = (By.CSS_SELECTOR, '#input-telephone')
+    PASSWORD_INPUT = (By.CSS_SELECTOR, '#input-password')
+    PASSWORD_CONFIRM_INPUT = (By.CSS_SELECTOR, '#input-confirm')
+    PRIVACY_CHECK = (By.CSS_SELECTOR, "input['value'='Login']")
+    CONTINUE = (By.CSS_SELECTOR, "input['type'='submit']")
+
+    def register_account(self, first_name, last_name, email, phone, password):
+        first_name_input = self.element(self.FIRST_NAME_INPUT)
+        self._input(first_name_input, first_name)
+
+        last_name_input = self.element(self.LAST_NAME_INPUT)
+        self._input(last_name_input, last_name)
+
+        email_input = self.element(self.EMAIL_INPUT)
+        self._input(email_input, email)
+
+        phone_input = self.element(self.PHONE_INPUT)
+        self._input(phone_input, phone)
+        
+        password_input = self.element(self.PASSWORD_INPUT)
+        self._input(password_input, password)
+
+        password_confirm_input = self.element(self.PASSWORD_CONFIRM_INPUT)
+        self._input(password_confirm_input, password)
+
+        privacy_check = self.element(self.PRIVACY_CHECK)
+        self.click(privacy_check)
+
+        self.click(self.element(self.CONTINUE))
+
+class AfterRegisterPage(BasePage):
+    SUCCESS_NOTE = (By.CSS_SELECTOR, '#content > h1')
+
 
 class CartPage(BasePage):
     BUTTONS = (By.CSS_SELECTOR, ".buttons")
@@ -68,6 +148,7 @@ class ComparisonPage(BasePage):
 
 
 class MainPage(BasePage):
+    IPHONE = (By.LINK_TEXT, "iPhone")
     EMPTY_CART_INFO = (By.XPATH, "//div[contains(text(), 'Your shopping cart is empty!')]")
     CART = (By.CSS_SELECTOR, '#cart > button')
     SLIDE_IMAGE_NEXT = (By.CLASS_NAME, "swiper-button-next")
@@ -76,6 +157,24 @@ class MainPage(BasePage):
     PRODUCT_NAME = (By.CSS_SELECTOR, ".caption h4 a")
     PRODUCT_IMAGE = (By.CSS_SELECTOR, ".product-layeout > div.product-thumb > div.image")
     WISH_BUTTON = (By.CSS_SELECTOR, "button['data-original-title'='Add to Wish List']")
+    NAVBAR = (By.CSS_SELECTOR, "ul.nav.navbar-nav")
+    DESKTOPS = (By.LINK_TEXT, "Desktops")
+    ALL_DESKTOPS = (By.LINK_TEXT, "Show All Desktops")
+    MY_ACCOUNT = (By.LINK_TEXT, "My Account")
+    LOGIN = (By.LINK_TEXT, "Login")
+    REGISTER = (By.LINK_TEXT, "Register")
+
+    DESKTOPS = (By.LINK_TEXT, "Desktops")
+    LAPTOPS = (By.LINK_TEXT, "Laptops & Notebooks")
+    COMPONENTS = (By.LINK_TEXT, "Components")
+    TABLETS = (By.LINK_TEXT, "Tablets")
+    SOFTWARE = (By.LINK_TEXT, "Software")
+    PHONES = (By.LINK_TEXT, "Phones & PDAs")
+    CAMERAS = (By.LINK_TEXT, "Cameras")
+    MP3_PLAYERS = (By.LINK_TEXT, "MP3 Players")
+
+    SLIDE_ACTIVE_ELEMENT = (By.CSS_SELECTOR, "div.swiper-wrapper > div.swiper-slide-duplicate-active")
+
 
     def slide_image_next(self):
         next_button = self.element(self.SLIDE_IMAGE_NEXT)
@@ -91,7 +190,16 @@ class MainPage(BasePage):
 
     def wish_featured_product(self, index):
         wish_button = self.elements(self.FEATURED_PRODUCT)[index].find_element(*self.WISH_BUTTON)
-        self.click(wish_button) 
+        self.click(wish_button)
+
+    def click_login(self):
+        self.click(self.element(self.MY_ACCOUNT))
+        self.click(self.element(self.LOGIN))
+
+    def click_register(self):
+        self.click(self.element(self.MY_ACCOUNT))
+        self.click(self.element(self.REGISTER))
+
 
 
 
@@ -99,6 +207,11 @@ class ProductPage(BasePage):
     WISH_LIST = (By.CSS_SELECTOR, "[data-original-title='Add to Wish List']")
     COMPARE = (By.CSS_SELECTOR, "[data-original-title='Compare this Product']")
     CART = (By.CSS_SELECTOR, "#button-cart")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "#content > div > div > h1")
+    RELATED_PRODUCTS_HEAD = (By.CSS_SELECTOR, '#content > h3')
+    RELATED_PRODUCT = (By.CSS_SELECTOR, '#div.product-thumb')
+    RELATED_PRODUCT_IMAGE_REF = (By.CSS_SELECTOR, "div.image > a")
+    RELATED_PRODUCT_IMAGE = (By.CSS_SELECTOR, "div.image > a > img")
 
     def add_to_wish_list(self):
         self.click(self.element(self.WISH_LIST))
@@ -109,6 +222,9 @@ class ProductPage(BasePage):
     def add_to_cart(self):
         time.sleep(1.5)  # Page loading problem
         self.click(self.element(self.CART))
+
+    def get_product_name(self):
+        self.element(self.PRODUCT_NAME).text
 
 
 class UserPage(BasePage):
@@ -139,6 +255,7 @@ class AlertElement(BasePage):
     COMPARISON = (By.LINK_TEXT, "product comparison")
     LOGIN = (By.LINK_TEXT, "login")
     CART = (By.LINK_TEXT, "shopping cart")
+    UNSUCCESS_LOGIN = (By.CSS_SELECTOR, "#account-login > div.alert.alert-danger.alert-dismissible")
 
     def __init__(self, driver):
         super().__init__(driver=driver)
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,78 +1,78 @@
+from re import M
 import pytest
+import time
+import random
 from conftest import driver, url
+from PageObjects import MainPage, AlertElement
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from exception_wrappers import *
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 def test_home_button(driver, url):
     driver.get(url=url)
-    home_button = driver.find_element(by=By.CSS_SELECTOR, value='#logo > a')
-    assert home_button.get_attribute('href') == 'http://192.168.0.102:8081/index.php?route=common/home'
+    home_button = MainPage(driver).element(By.CSS_SELECTOR, '#logo > a')
+    assert home_button.get_attribute('href') == url+'/index.php?route=common/home'
 
 
 def test_navbar_elements(driver, url):
     driver.get(url=url)
-    navbar = driver.find_element(by=By.CSS_SELECTOR, value=".collapse.navbar-collapse>.nav.navbar-nav")
-    navbar_elems = navbar.find_elements(by=By.CSS_SELECTOR, value=".nav.navbar-nav > li")
-    assert len(navbar_elems)==8
-    
-    desktops_ref = navbar_elems[0].find_element(by=By.TAG_NAME, value="a")
-    href_result = desktops_ref.get_attribute("href")
-    href_expectation = url+"/desktops"
-    assert href_result == href_expectation
-    assert desktops_ref.text == "Desktops"    
-    
-    mp3players_ref = navbar_elems[-1].find_element(by=By.TAG_NAME, value="a")
-    href_result = mp3players_ref.get_attribute("href")
-    href_expectation = url+"/mp3-players"
-    assert href_result == href_expectation
-    assert mp3players_ref.text == "MP3 Players"
-    
-    navbar_elems[4].click()
-    left_column_software_ref = wait((By.XPATH, '//*[@id="column-left"]/div[1]/a[5]'), driver)
-    assert left_column_software_ref.text.startswith('Software')
-    assert left_column_software_ref.get_attribute('href').startswith(url+'/software')
-    assert 'active' in left_column_software_ref.get_attribute('class')
-    driver.back()
-    navbar = driver.find_element(by=By.CSS_SELECTOR, value=".collapse.navbar-collapse>.nav.navbar-nav")
-    navbar_elems = navbar.find_elements(by=By.CSS_SELECTOR, value=".nav.navbar-nav > li")
+    navbar = MainPage(driver).element(MainPage.NAVBAR)
+    desktops_button = MainPage(driver).element_in_element(navbar, MainPage.DESKTOPS)
+    assert desktops_button.get_attribute("href")==url+"/desktops"
 
-    wait((By.CSS_SELECTOR, "#slideshow0"), driver)
-    navbar_elems[5].click()
-    left_column_phones_pdas_ref = wait((By.XPATH, '//*[@id="column-left"]/div[1]/a[6]'), driver)
-    assert left_column_phones_pdas_ref.text.startswith('Phones & PDAs')
-    assert left_column_phones_pdas_ref.get_attribute('href').startswith(url+'/smartphone')
-    assert 'active' in left_column_phones_pdas_ref.get_attribute('class')
+    laptops_button = MainPage(driver).element_in_element(navbar, MainPage.LAPTOPS)
+    assert laptops_button.get_attribute("href")==url+"/laptop-notebook"
+
+    components_button = MainPage(driver).element_in_element(navbar, MainPage.COMPONENT)
+    assert components_button.get_attribute("href")==url+"/component"
+
+    tablets_button = MainPage(driver).element_in_element(navbar, MainPage.TABLETS)
+    assert tablets_button.get_attribute("href")==url+"/tablet"
+
+    software_button = MainPage(driver).element_in_element(navbar, MainPage.SOFTWARE)
+    assert software_button.get_attribute("href")==url+"/software"
+
+    phone_pda_button = MainPage(driver).element_in_element(navbar, MainPage.PHONES)
+    assert phone_pda_button.get_attribute("href")==url+"/smartphone"
+
+    cameras_button = MainPage(driver).element_in_element(navbar, MainPage.CAMERAS)
+    assert cameras_button.get_attribute("href")==url+"/camera"
+
+    cameras_button = MainPage(driver).element_in_element(navbar, MainPage.MP3_PLAYERS)
+    assert cameras_button.get_attribute("href")==url+"/mp3-player"
+
+
+def test_slide_button(driver, url):
+    driver.get(url=url)
+    old_element_id = MainPage(driver).element(MainPage.SLIDE_ACTIVE_ELEMENT)\
+        .get_attribute("data-swiper-slide-index")
+    MainPage(driver).slide_image_next()
+    time.sleep(2)
+    new_element_id = MainPage(driver).element(MainPage.SLIDE_ACTIVE_ELEMENT)\
+        .get_attribute("data-swiper-slide-index")
+    assert old_element_id!=new_element_id
 
 
 def test_featured_elements(driver, url):
     driver.get(url=url)
-    featured_elems = driver.find_elements(by=By.CSS_SELECTOR, value='#content > div.row > div.product-layout')
-    assert len(featured_elems)==4
-    for i, elem in enumerate(featured_elems):
-        ref_image = wait((By.CSS_SELECTOR, 'div > div.image > a'), driver)
-        ActionChains(driver).move_to_element(ref_image).click().perform()
-        add_to_cart_button = wait((By.CSS_SELECTOR, '#button-cart'), driver)
-        assert add_to_cart_button.text=='Add to Cart'
-        driver.back()
-        wait((By.CSS_SELECTOR, '#content > div.row > div.product-layout'), driver)
-        wish_list_add_button = driver.find_element(by=By.CSS_SELECTOR, value=f'#content > div.row > div:nth-child({i+1}) > div > div.button-group > button:nth-child(2)')
-        wish_list_add_button.click()
-        alert = wait((By.CSS_SELECTOR, '#common-home > div.alert.alert-success.alert-dismissible'), driver)
-        
-        assert "You must" in alert.text
-        alert_close_button = driver.find_element(by=By.CSS_SELECTOR, value='#common-home > div.alert.alert-success.alert-dismissible > button')
-        alert_close_button.click()
-        wait_not((By.CSS_SELECTOR, '#common-home > div.alert.alert-success.alert-dismissible > button'), driver)
+    featured_elements = MainPage(driver).elements(MainPage.FEATURED_PRODUCT)
+    choice_index = random.randint(0,len(featured_elements)-1)
+    MainPage(driver).wish_featured_product(choice_index)
+    MainPage(driver).element()
+    AlertElement(driver).wish()
+    MainPage(driver).wait(EC.element_to_be_clickable, MainPage.PRODUCT_IMAGE)
 
 
 def test_basket(driver, url):
     driver.get(url=url)
-    basket_button = driver.find_element(by=By.CSS_SELECTOR, value='#cart > button')
-    if basket_button.text=="0 item(s) - $0.00":
-        basket_button.click()
-        notification = wait((By.CSS_SELECTOR, '#cart > ul > li > p'), driver)
-        assert notification.text == 'Your shopping cart is empty!'
+    basket_button = MainPage(driver).element(MainPage.CART)
+    MainPage(driver).click(basket_button)
+    assert "open" in basket_button.get_attribute('class')
+
+
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path=driver_folder+'/chromedriver'+extension, options=options)
+url = r"http://192.168.0.102:8081"
+test_home_button(driver, url)
 
 
